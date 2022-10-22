04a4bc87f52f787be64ea4597047f2df022409b6
rotaruteodor <rotaruteodorgabriel@gmail.com>
2022-10-22 09:08:13 +0300

    Initial commit

---
+++ src/main/backend/web/spring/flowershop/FlowershopApplication.java
@@ -0,0 +1,13 @@
+package web.spring.flowershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowershopApplication.class, args);
+	}
+
+}
